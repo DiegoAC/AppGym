6e67e80f701a98367b8d17ec5216b1f17d363318
DiegoAC <diegoasen3@gmail.com>
2025-06-15 23:06:45 +0200

    Agregar archivos de proyecto.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppGym
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
